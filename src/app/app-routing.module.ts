2303a41f6ac8080c41e3bfc9a70d33b0ad11eef7
Alessandro Duarte Junior <alessandroduartejunior@gmail.com>
2018-06-30 18:09:48 -0300

    telas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { ListagemComponent } from './pages/listagem/listagem.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'listagem', component: ListagemComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { ListagemComponent } from './pages/listagem/listagem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormularioComponent,
+    ListagemComponent
   ],
   imports: [
     BrowserModule,
