6d1e6dee4c2628f0ff8cbec47ddeaece4f6d8f4f
Alessandro Duarte Junior <alessandroduartejunior@gmail.com>
2018-07-01 15:48:37 -0300

    ajuste deploy

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require('express'),
+    path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use(express.static('./dist/spring-boot-angular-front'));
+
+app.get('/*', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '/dist/spring-boot-angular-front/index.html'));
+
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server started');
+})
